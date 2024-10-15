7cf55c0e0e7eaf9b98d44d1fe95538c4a6c3ed3c
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-10-15 12:47:58 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- lib/src/packet/signature/notation_data.dart
+++ lib/src/packet/signature/notation_data.dart
@@ -7,6 +7,7 @@ import 'dart:math';
 import 'dart:typed_data';
 
 import '../../enum/signature_subpacket_type.dart';
+import '../../helpers.dart';
 import '../signature_subpacket.dart';
 
 /// This subpacket describes a "notation" on the signature that the issuer wishes to make.
@@ -14,12 +15,13 @@ import '../signature_subpacket.dart';
 /// See https://www.rfc-editor.org/rfc/rfc4880#section-5.2.3.16
 /// Author Nguyen Van Nguyen <nguyennv1981@gmail.com>
 class NotationData extends SignatureSubpacket {
+  static const saltName = "salt@notations.dart-pg.org";
+
   static const headerFlagLength = 4;
   static const headerNameLength = 2;
   static const headerValueLength = 2;
 
-  NotationData(final Uint8List data, {super.critical, super.isLong})
-      : super(SignatureSubpacketType.notationData, data);
+  NotationData(final Uint8List data, {super.critical, super.isLong}) : super(SignatureSubpacketType.notationData, data);
 
   factory NotationData.fromNotation(
     final bool humanReadable,
@@ -32,23 +34,40 @@ class NotationData extends SignatureSubpacket {
         critical: critical,
       );
 
+  factory NotationData.saltNotation(
+    int saltSize, {
+    final bool critical = false,
+  }) {
+    final salt = Helper.secureRandom().nextBytes(saltSize);
+    final nameData = utf8.encode(saltName);
+    final nameLength = min(nameData.length, 0xffff);
+    return NotationData(
+      Uint8List.fromList([
+        ...[0, 0, 0, 0],
+        (nameLength >> 8) & 0xff,
+        (nameLength >> 0) & 0xff,
+        (salt.length >> 8) & 0xff,
+        (salt.length >> 0) & 0xff,
+        ...nameData,
+        ...salt,
+      ]),
+      critical: critical,
+    );
+  }
+
   bool get isHumanReadable => data[0] == 0x80;
 
   String get notationName {
-    final nameLength = (((data[headerFlagLength] & 0xff) << 8) +
-        (data[headerFlagLength + 1] & 0xff));
+    final nameLength = (((data[headerFlagLength] & 0xff) << 8) + (data[headerFlagLength + 1] & 0xff));
     final nameOffset = headerFlagLength + headerNameLength + headerValueLength;
     return utf8.decode(data.sublist(nameOffset, nameOffset + nameLength));
   }
 
   String get notationValue {
-    final nameLength = (((data[headerFlagLength] & 0xff) << 8) +
-        (data[headerFlagLength + 1] & 0xff));
-    final valueLength =
-        (((data[headerFlagLength + headerNameLength] & 0xff) << 8) +
-            (data[headerFlagLength + headerNameLength + 1] & 0xff));
-    final valueOffset =
-        headerFlagLength + headerNameLength + headerValueLength + nameLength;
+    final nameLength = (((data[headerFlagLength] & 0xff) << 8) + (data[headerFlagLength + 1] & 0xff));
+    final valueLength = (((data[headerFlagLength + headerNameLength] & 0xff) << 8) +
+        (data[headerFlagLength + headerNameLength + 1] & 0xff));
+    final valueOffset = headerFlagLength + headerNameLength + headerValueLength + nameLength;
     return utf8.decode(data.sublist(valueOffset, valueOffset + valueLength));
   }
 
