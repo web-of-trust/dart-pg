6bd8d0f766a5911e927370bf4b1411df02ce62c4
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-11-21 13:08:28 +0700

    Use record for Key.readPacketList
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- lib/src/type/key.dart
+++ lib/src/type/key.dart
@@ -245,7 +245,13 @@ abstract class Key {
         ...subkeys.map((subkey) => subkey.toPacketList()).expand((packet) => packet),
       ]);
 
-  static Map<String, dynamic> readPacketList(final PacketList packetList) {
+  static ({
+    KeyPacket? keyPacket,
+    List<User> users,
+    List<SignaturePacket> revocationSignatures,
+    List<SignaturePacket> directSignatures,
+    List<Subkey> subkeys,
+  }) readPacketList(final PacketList packetList) {
     final revocationSignatures = <SignaturePacket>[];
     final directSignatures = <SignaturePacket>[];
     final users = <User>[];
@@ -351,12 +357,12 @@ abstract class Key {
       throw StateError('Key packet not found in packet list');
     }
 
-    return {
-      'keyPacket': keyPacket,
-      'users': users,
-      'revocationSignatures': revocationSignatures,
-      'directSignatures': directSignatures,
-      'subkeys': subkeys,
-    };
+    return (
+      keyPacket: keyPacket,
+      users: users,
+      revocationSignatures: revocationSignatures,
+      directSignatures: directSignatures,
+      subkeys: subkeys,
+    );
   }
 }
