0dbabbf7fb00175735b81228dbe2850aa50ba913
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-11-25 16:04:47 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- lib/src/enum/profile.dart
+++ lib/src/enum/profile.dart
@@ -7,8 +7,13 @@ library;
 /// OpenPGP profiles enum
 /// Author Nguyen Van Nguyen <nguyennv1981@gmail.com>
 enum Profile {
+  /// RFC 4880 & 5581 & 6637 profile
+  /// https://www.rfc-editor.org/rfc/rfc4880
+  /// https://www.rfc-editor.org/rfc/rfc5581
+  /// https://www.rfc-editor.org/rfc/rfc6637
   rfc4880,
-  rfc5581,
-  rfc6637,
+
+  /// RFC 9580 profile
+  /// https://www.rfc-editor.org/rfc/rfc9580
   rfc9580;
 }
