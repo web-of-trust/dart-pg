45aa3d6dd24cd75890eafd37aedd73306111ce8b
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-11-26 15:23:09 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- test/packet/encryption_test.dart
+++ test/packet/encryption_test.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:dart_pg/src/common/argon2_s2k.dart';
 import 'package:dart_pg/src/common/helpers.dart';
 import 'package:dart_pg/src/enum/aead_algorithm.dart';
 import 'package:dart_pg/src/enum/symmetric_algorithm.dart';
@@ -44,7 +47,142 @@ void main() {
     });
   });
 
-  group('Symmetrically decryption', () {});
+  group('Symmetrically decryption', () {
+    const passphrase = 'password';
+    const literalText = "Hello, world!";
+
+    test('Encrypted using aead eax', () {
+      final skesk = SymEncryptedSessionKeyPacket.fromBytes(base64.decode(
+        'Bh4HAQsDCKWuV50fxdgr/2kiT5GZk7NQb6O1mmpzz/jF78X0HFf7VOHCJoFdeCj1+SxFTrZevgCrWYbGjm58VQ==',
+      )).decrypt(passphrase);
+      expect(skesk.symmetric, SymmetricAlgorithm.aes128);
+      expect(skesk.aead, AeadAlgorithm.eax);
+
+      final sessionKey = skesk.sessionKey!;
+      expect(
+        sessionKey.encryptionKey,
+        '3881bafe985412459b86c36f98cb9a5e'.hexToBytes(),
+      );
+
+      final seipd = SymEncryptedIntegrityProtectedDataPacket.fromBytes(base64.decode(
+        'AgcBBp/5DjsyGWTzpCkTyNzGYZMlAVIn77fq6qSfBMLmdBddSj0ibtavy5yprBIsFHDhHGPUwKskHGqTitSL+ZpambkLuoMl3mEEdUAlireVmpWtBR3alusVQx3+9fXiJVyngmFUbjOa',
+      )).decrypt(sessionKey.encryptionKey);
+      expect(seipd.symmetric, SymmetricAlgorithm.aes128);
+      expect(seipd.aead, AeadAlgorithm.eax);
+
+      final literalData = seipd.packets!.elementAt(0) as LiteralDataInterface;
+      expect(literalData.binary, literalText.toBytes());
+    });
+
+    test('Encrypted using aead ocb', () {
+      final skesk = SymEncryptedSessionKeyPacket.fromBytes(base64.decode(
+        'Bh0HAgsDCFaimNL142RT/8/MXBFmTtudtCWQ19xGsHJBthLDgSz/++oA8jR7JWQRI/iHrmDU/WFOCDfYGdNs',
+      )).decrypt(passphrase);
+      expect(skesk.symmetric, SymmetricAlgorithm.aes128);
+      expect(skesk.aead, AeadAlgorithm.ocb);
+
+      final sessionKey = skesk.sessionKey!;
+      expect(
+        sessionKey.encryptionKey,
+        '28e79ab82397d3c63de24ac217d7b791'.hexToBytes(),
+      );
+
+      final seipd = SymEncryptedIntegrityProtectedDataPacket.fromBytes(base64.decode(
+        'AgcCBiCmYfcx/JowMrViMyYCfjpdjbV0jr7/CwxZENCezdZB/5/ThWJ1gDW8SXVM4b8//6fa0KO4EE9RM89CpBAKg+70yhtIAaiEa/QrzafIzp1l4hLzAcvNmP3K3mlKh3rUJHMj9uhX',
+      )).decrypt(sessionKey.encryptionKey);
+      expect(seipd.symmetric, SymmetricAlgorithm.aes128);
+      expect(seipd.aead, AeadAlgorithm.ocb);
+
+      final literalData = seipd.packets!.elementAt(0) as LiteralDataInterface;
+      expect(literalData.binary, literalText.toBytes());
+    });
+
+    test('Encrypted using aead gcm', () {
+      final skesk = SymEncryptedSessionKeyPacket.fromBytes(base64.decode(
+        'BhoHAwsDCOnTl4WyBwAI/7QufEg+9IhEV8s3Jrmz25/3duX02aQJUuJEcpiFGr//dSbfLdVUQXV5p3mf',
+      )).decrypt(passphrase);
+      expect(skesk.symmetric, SymmetricAlgorithm.aes128);
+      expect(skesk.aead, AeadAlgorithm.gcm);
+
+      final sessionKey = skesk.sessionKey!;
+      expect(
+        sessionKey.encryptionKey,
+        '1936fc8568980274bb900d8319360c77'.hexToBytes(),
+      );
+
+      final seipd = SymEncryptedIntegrityProtectedDataPacket.fromBytes(base64.decode(
+        'AgcDBvy5RJC8uYu9ydEGxgkCZpQPcuie3CG1WWsVdrEB7Q+f/G/G1lu/0k3NB5CWbm0ehaMAU3hMsdi2oGme8SFVp7KtYlhTG1dlH9d3eRL6leNdm0Ahb2mkwkjbKP9DMfFjKQc5nm/5',
+      )).decrypt(sessionKey.encryptionKey);
+      expect(seipd.symmetric, SymmetricAlgorithm.aes128);
+      expect(seipd.aead, AeadAlgorithm.gcm);
+
+      final literalData = seipd.packets!.elementAt(0) as LiteralDataInterface;
+      expect(literalData.binary, literalText.toBytes());
+    });
+
+    test('V4 SKESK Using Argon2 with AES-128', () {
+      final skesk = SymEncryptedSessionKeyPacket.fromBytes(base64.decode(
+        'BAcEnFL4PCf5XlDVNUQOzf8xNgEEFZ5S/K0izz+VZULLp5TvhAsR',
+      )).decrypt(passphrase);
+      expect(skesk.symmetric, SymmetricAlgorithm.aes128);
+      expect(skesk.s2k is Argon2S2k, isTrue);
+
+      final sessionKey = skesk.sessionKey!;
+      expect(
+        sessionKey.encryptionKey,
+        '01fe16bbacfd1e7b78ef3b865187374f'.hexToBytes(),
+      );
+      expect(sessionKey.symmetric, SymmetricAlgorithm.aes128);
+
+      final seipd = SymEncryptedIntegrityProtectedDataPacket.fromBytes(base64.decode(
+        'AZgYpj5gnPi7oX4MOUME6vk1FBe38okh/ibiY6UrIL+6otumcslkydOrejv0bEFN0h07OEdd8DempXiZPMU=',
+      )).decrypt(sessionKey.encryptionKey, symmetric: sessionKey.symmetric);
+      final literalData = seipd.packets!.elementAt(0) as LiteralDataInterface;
+      expect(literalData.binary, literalText.toBytes());
+    });
+
+    test('V4 SKESK Using Argon2 with AES-192', () {
+      final skesk = SymEncryptedSessionKeyPacket.fromBytes(base64.decode(
+        'BAgE4UysRxU0WRipYtyjR+FD+AEEFYcyydr2txRvP6ZqSD3fx/5naFUuVQSy8Bc=',
+      )).decrypt(passphrase);
+      expect(skesk.symmetric, SymmetricAlgorithm.aes192);
+      expect(skesk.s2k is Argon2S2k, isTrue);
+
+      final sessionKey = skesk.sessionKey!;
+      expect(
+        sessionKey.encryptionKey,
+        '27006dae68e509022ce45a14e569e91001c2955af8dfe194'.hexToBytes(),
+      );
+      expect(sessionKey.symmetric, SymmetricAlgorithm.aes192);
+
+      final seipd = SymEncryptedIntegrityProtectedDataPacket.fromBytes(base64.decode(
+        'AdJ1Sw56PRYiKZjCvHg+2bnq02s33AJJoyBexBI4QKATFRkyez2gldJldRysLVg77Mwwfgl2n/d572WciAM=',
+      )).decrypt(sessionKey.encryptionKey, symmetric: sessionKey.symmetric);
+      final literalData = seipd.packets!.elementAt(0) as LiteralDataInterface;
+      expect(literalData.binary, literalText.toBytes());
+    });
+
+    test('V4 SKESK Using Argon2 with AES-256', () {
+      final skesk = SymEncryptedSessionKeyPacket.fromBytes(base64.decode(
+        'BAkEuHiVICBv95nGiCxCRaZifAEEFZ2fZeyrWoHQpZvVGkP2ejP+a6JJUhqRrutt2Jml3sxo/A==',
+      )).decrypt(passphrase);
+      expect(skesk.symmetric, SymmetricAlgorithm.aes256);
+      expect(skesk.s2k is Argon2S2k, isTrue);
+
+      final sessionKey = skesk.sessionKey!;
+      expect(
+        sessionKey.encryptionKey,
+        'bbeda55b9aae63dac45d4f49d89dacf4af37fefc13bab2f1f8e18fb74580d8b0'.hexToBytes(),
+      );
+      expect(sessionKey.symmetric, SymmetricAlgorithm.aes256);
+
+      final seipd = SymEncryptedIntegrityProtectedDataPacket.fromBytes(base64.decode(
+        'AfirtbIE3SaPO19Vq7qe5dMCcqWZbNtVMHeu5vZKBetHnnx/yveQ9brJYlzhJvGskCUJma43+iur/T1sKjE=',
+      )).decrypt(sessionKey.encryptionKey, symmetric: sessionKey.symmetric);
+      final literalData = seipd.packets!.elementAt(0) as LiteralDataInterface;
+      expect(literalData.binary, literalText.toBytes());
+    });
+  });
 
   group('Symmetrically encryption', () {
     final literalData = LiteralDataPacket.fromText(
