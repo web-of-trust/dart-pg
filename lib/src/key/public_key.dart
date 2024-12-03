12b6758c80c3aa356a56e99e053641caf7c8d1a9
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2024-12-03 10:56:00 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- lib/src/key/public_key.dart
+++ lib/src/key/public_key.dart
@@ -4,10 +4,13 @@
 
 library;
 
-import 'package:dart_pg/src/common/armor.dart';
-import 'package:dart_pg/src/enum/armor_type.dart';
-import 'package:dart_pg/src/packet/packet_list.dart';
+import 'dart:typed_data';
 
+import 'package:dart_pg/src/packet/base.dart';
+
+import '../common/armor.dart';
+import '../enum/armor_type.dart';
+import '../packet/packet_list.dart';
 import '../type/key.dart';
 import 'base.dart';
 
@@ -16,6 +19,7 @@ import 'base.dart';
 final class PublicKey extends Base {
   PublicKey(super.packetList);
 
+  /// Read public key from armored string
   factory PublicKey.fromArmored(final String armored) {
     final armor = Armor.decode(armored);
     if (armor.type != ArmorType.publicKey) {
@@ -23,6 +27,42 @@ final class PublicKey extends Base {
     }
     return PublicKey(PacketList.decode(armor.data));
   }
+
+  /// Read public keys from armored string
+  /// Return one or multiple key objects
+  static Iterable<KeyInterface> readPublicKeys(final String armored) {
+    final armor = Armor.decode(armored);
+    if (armor.type != ArmorType.publicKey) {
+      throw ArgumentError('Armored text not of public key type');
+    }
+    final publicKeys = <KeyInterface>[];
+    final packetList = PacketList.decode(armor.data);
+    final indexes = packetList.indexOfTypes([PacketType.publicKey]);
+    for (var i = 0; i < indexes.length; i++) {
+      if (indexes.asMap().containsKey(i + 1)) {
+        publicKeys.add(
+          PublicKey(
+            PacketList(
+              packetList.packets.sublist(indexes[i], indexes[i + 1]),
+            ),
+          ),
+        );
+      }
+    }
+    return publicKeys;
+  }
+
+  /// Armor multiple public key.
+  static String armorPublicKeys(Iterable<KeyInterface> keys) => Armor.encode(
+      ArmorType.publicKey,
+      Uint8List.fromList(keys
+          .map(
+            (key) => key.publicKey.packetList,
+          )
+          .expand(
+            (packet) => packet.encode(),
+          )
+          .toList()));
 
   @override
   String armor() => Armor.encode(ArmorType.publicKey, packetList.encode());
